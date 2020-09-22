8af9a7a478d72766f72d99c40396b26d8c436a6f
saif raja <sraja@qa.com>
2020-09-22 15:29:57 +0100

    first commit

---
+++ src/blackjack/runner.java
@@ -0,0 +1,16 @@
+package blackjack;
+
+public class runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		        BlackJack b = new BlackJack();
+		        System.out.println(b.play(12, 19));
+		    }
+		
+		
+		
+	}
+
+
